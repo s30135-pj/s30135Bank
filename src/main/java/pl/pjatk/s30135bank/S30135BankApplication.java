e4347bb20dd7d4a703c486c99026043607c462e7
Władysław Farat <s30135@pjwstk.edu.pl>
2025-01-25 15:35:14 +0100

    First commit

---
+++ src/main/java/pl/pjatk/s30135bank/S30135BankApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.s30135bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S30135BankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S30135BankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/pjatk/s30135bank/S30135BankApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.s30135bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S30135BankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
